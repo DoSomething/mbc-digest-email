e574fd32e3bc46b7484d841d64ed03ed9c82a779
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-05-28 14:08:24 -0400

    First draft

---
+++ mbc-digest-email.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * mbc-user-digest.php
+ *
+ * A consumer to process entries in the userDigestQueue via the directUserDigest
+ * exchange. The mbp-user-digest application produces the entries in the
+ * queue.
+ */
+
+// Load up the Composer autoload magic
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load configuration settings common to the Message Broker system
+// symlinks in the project directory point to the actual location of the files
+require __DIR__ . '/mb-secure-config.inc';
+require __DIR__ . '/mb-config.inc';
+
+require __DIR__ . '/MBC_UserDigest.class.inc';
+
+// Settings
+$credentials = array(
+  'host' =>  getenv("RABBITMQ_HOST"),
+  'port' => getenv("RABBITMQ_PORT"),
+  'username' => getenv("RABBITMQ_USERNAME"),
+  'password' => getenv("RABBITMQ_PASSWORD"),
+  'vhost' => getenv("RABBITMQ_VHOST"),
+);
+$config = array(
+  'exchange' => array(
+    'name' => getenv("MB_USER_DIGEST_EXCHANGE"),
+    'type' => getenv("MB_USER_DIGEST_EXCHANGE_TYPE"),
+    'passive' => getenv("MB_USER_DIGEST_EXCHANGE_PASSIVE"),
+    'durable' => getenv("MB_USER_DIGEST_EXCHANGE_DURABLE"),
+    'auto_delete' => getenv("MB_USER_DIGEST_EXCHANGE_AUTO_DELETE"),
+  ),
+  'queue' => array(
+    array(
+      'name' => getenv("MB_USER_DIGEST_QUEUE"),
+      'passive' => getenv("MB_USER_DIGEST_QUEUE_PASSIVE"),
+      'durable' => getenv("MB_USER_DIGEST_QUEUE_DURABLE"),
+      'exclusive' => getenv("MB_USER_DIGEST_QUEUE_EXCLUSIVE"),
+      'auto_delete' => getenv("MB_USER_DIGEST_QUEUE_AUTO_DELETE"),
+      'bindingKey' => getenv("MB_USER_DIGEST_QUEUE_TOPIC_MB_TRANSACTIONAL_EXCHANGE_PATTERN"),
+    ),
+  ),
+);
+$settings = array(
+  'mandrill' => '',
+  'stathat_ez_key' => getenv("STATHAT_EZKEY"),
+);
+
+echo '------- mbc-user-digest START: ' . date('D M j G:i:s T Y') . ' -------', "\n";
+
+// Kick off
+$mbcDigestEmail = new MBC_DigestEmail($credentials, $config, $settings);
+
+// Process digest message requests by mbp-user-digest
+$mbcDigestEmail->consumeUserDigestQueue();
+
+echo '------- mbc-user-digest END: ' . date('D M j G:i:s T Y') . ' -------', "\n";
