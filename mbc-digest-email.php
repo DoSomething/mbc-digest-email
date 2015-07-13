4b4cd029b17ffbcc39dc7bbd0a3f743d9b35cf87
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-07-12 22:10:10 -0400

    Debugging for duplicate sending of digest message

--- mbc-digest-email.php
+++ mbc-digest-email.php
@@ -18,15 +18,43 @@ use DoSomething\MB_Toolbox\MB_Configuration;
 // Load configuration settings common to the Message Broker system
 // symlinks in the project directory point to the actual location of the files
 require_once __DIR__ . '/messagebroker-config/mb-secure-config.inc';
+require_once __DIR__ . '/MBC_RabbitMQManagementAPI.class.inc';
 require_once __DIR__ . '/MBC_UserDigest.class.inc';
 
 // Settings
 $credentials = array(
-  'host' =>  getenv("RABBITMQ_HOST"),
-  'port' => getenv("RABBITMQ_PORT"),
-  'username' => getenv("RABBITMQ_USERNAME"),
-  'password' => getenv("RABBITMQ_PASSWORD"),
-  'vhost' => getenv("RABBITMQ_VHOST"),
+  'rabbit' => array(
+    'host' =>  getenv("RABBITMQ_HOST"),
+    'port' => getenv("RABBITMQ_PORT"),
+    'username' => getenv("RABBITMQ_USERNAME"),
+    'password' => getenv("RABBITMQ_PASSWORD"),
+    'vhost' => getenv("RABBITMQ_VHOST"),
+  ),
+  'rabbitManagementAPI' => array(
+    'host' => getenv("MB_RABBITMQ_MANAGEMENT_API_HOST"),
+    'port' => getenv("MB_RABBITMQ_MANAGEMENT_API_PORT"),
+    'username' => getenv("MB_RABBITMQ_MANAGEMENT_API_USERNAME"),
+    'password' => getenv("MB_RABBITMQ_MANAGEMENT_API_PASSWORD"),
+  ),
+  'stathat' => array(
+    'ez_key' => getenv("STATHAT_EZKEY"),
+    'use_stathat_tracking' => getenv("USE_STAT_TRACKING"),
+  ),
+  'ds_drupal_api' => array(
+    'host' => getenv('DS_DRUPAL_API_HOST'),
+    'port' => getenv('DS_DRUPAL_API_PORT'),
+    'username' => getenv("DS_DRUPAL_API_USERNAME"),
+    'password' => getenv("DS_DRUPAL_API_PASSWORD"),
+  ),
+  'mb_user_api' => array(
+    'host' =>  getenv("MB_USER_API_HOST"),
+    'port' =>  getenv("MB_USER_API_PORT"),
+  ),
+  'subscriptions' => array(
+    'url' => getenv("SUBSCRIPTIONS_URL"),
+    'ip' => getenv("SUBSCRIPTIONS_IP"),
+    'port' => getenv("SUBSCRIPTIONS_PORT"),
+  )
 );
 $settings = array(
   'stathat_ez_key' => getenv("STATHAT_EZKEY"),
@@ -41,6 +69,10 @@ $settings = array(
   'subscriptions_ip' => getenv("SUBSCRIPTIONS_IP"),
   'subscriptions_port' => getenv("SUBSCRIPTIONS_PORT"),
 );
+
+$mbcRabbitMQManagementAPI = new MBC_RabbitMQManagementAPI($credentials);
+$exchangeName = 'directUserDigestExchange';
+$targetQueue = $mbcRabbitMQManagementAPI->nextQueue($exchangeName);
 
 $config = array();
 $source = __DIR__ . '/messagebroker-config/mb_config.json';
@@ -57,23 +89,22 @@ $config = array(
   ),
   'queue' => array(
     array(
-      'name' => $userDigestExchange->queues->userDigestQueue->name,
+      'name' => $targetQueue,
       'passive' => $userDigestExchange->queues->userDigestQueue->passive,
       'durable' =>  $userDigestExchange->queues->userDigestQueue->durable,
       'exclusive' =>  $userDigestExchange->queues->userDigestQueue->exclusive,
       'auto_delete' =>  $userDigestExchange->queues->userDigestQueue->auto_delete,
-      'bindingKey' => $userDigestExchange->queues->userDigestQueue->binding_key,
+      'bindingKey' => $targetQueue,
     ),
   ),
   'consume' => array(
-    'consumer_tag' => $userDigestExchange->queues->userDigestQueue->consume->tag,
+    'consumer_tag' => $targetQueue,
     'no_local' => $userDigestExchange->queues->userDigestQueue->consume->no_local,
     'no_ack' => $userDigestExchange->queues->userDigestQueue->consume->no_ack,
     'exclusive' => $userDigestExchange->queues->userDigestQueue->consume->exclusive,
     'nowait' => $userDigestExchange->queues->userDigestQueue->consume->nowait,
   ),
 );
-
 
 echo '------- mbc-user-digest START: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
 
