7def693cf7649012a96f92df519fd996048f0207
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-19 16:39:16 -0400

    Remove script, no longer used

--- mbc-digest-email.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "mbc-digest-email.sh START"
-
-collectionName="digest-" + $(date+"%Y_%m_%d")
-mongo --host mongo4-aws --eval "rs.slaveOk(); use mb-users; db['mailchimp-users'].copyTo('$collectionName');"
-
-echo "mbc-digest-email.sh END"
