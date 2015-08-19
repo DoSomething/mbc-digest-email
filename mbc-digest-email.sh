e5eb0b0120e4eb632c409a210e20bc87b2d26c94
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-18 21:53:32 -0400

    Script update for cloning collection

--- mbc-digest-email.sh
+++ mbc-digest-email.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "mbc-digest-email.sh START"
 
-ssh mongo4
-collectionName="digest-" + $(date +"%Y_%m_%d")
-mongo --eval "db['mailchimp-users'].copyTo('$collectionName');"
+collectionName="digest-" + $(date+"%Y_%m_%d")
+mongo --host mongo4-aws --eval "rs.slaveOk(); use mb-users; db['mailchimp-users'].copyTo('$collectionName');"
 
 echo "mbc-digest-email.sh END"
