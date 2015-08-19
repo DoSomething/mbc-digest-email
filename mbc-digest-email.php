336a054bebd5c0a3a77a36e6ff700fde837efe04
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-19 17:01:50 -0400

    Restructure to use MB_Configuration as singleton

--- mbc-digest-email.php
+++ mbc-digest-email.php
@@ -12,23 +12,15 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBC_DigestEmail\MBC_DigestEmailConsumer;
+use DoSomething\MBC_DigestEmail\MBC_DigestEmail_Consumer;
 
-use DoSomething\StatHat\Client as StatHat;
-use DoSomething\MB_Toolbox\MB_Toolbox;
-
-// Load configuration settings specific to this application
 require_once __DIR__ . '/mbc-digest-email.config.inc';
-
-// Create objects for injection into MBC_ImageProcessor
-$mb = new MessageBroker($credentials['rabbit'], $config);
-$sh = new StatHat([
-  'ez_key' => $credentials['stathat']['stathat_ez_key'],
-  'debug' => $credentials['stathat']['stathat_disable_tracking']
-]);
-$tb = new MB_Toolbox($settings);
 
 
 echo '------- mbc-user-digest START: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
-$mb->consumeMessage(array(new MBC_DigestEmailConsumer($mb, $sh, $tb, $settings), 'consumeUserDigestQueue'));
+
+// Kick off
+$mb = $mbConfig->getProperty('messageBroker');
+$mb->consumeMessage(array(new MBC_DigestEmail_Consumer(), 'consumeUserDigestQueue'));
+
 echo '------- mbc-user-digest END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
