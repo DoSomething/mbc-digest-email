26c96a13ad6f6efa8a50581a36b129b32d95696f
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-20 20:26:59 -0400

    Gather preheader message markup

---
+++ src/MBC_DigestEmail_PreHeaderContent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MBC_DigestEmail_PreHeaderContent
+ * 
+ */
+
+namespace DoSomething\MBC_DigestEmail;
+
+
+/**
+ * MBC_DigestEmail_PreHeaderContent class - 
+ */
+class MBC_DigestEmail_PreHeaderContent extends MBC_DigestEmail_BaseContent {
+
+}
