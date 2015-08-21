b77db7bd25257621908904b6ee090a671b2db131
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-21 18:32:36 -0400

    Continue stubbing to supoort singleton instance settings

--- src/MBC_DigestEmail_Campaign.php
+++ src/MBC_DigestEmail_Campaign.php
@@ -10,4 +10,44 @@ namespace DoSomething\MBC_DigestEmail;
  */
 class MBC_DigestEmail_Campaign {
 
+  /**
+   * Singleton instance of application configuration settings.
+   *
+   * @var object
+   */
+   private $mbConfig;
+
+  /**
+   * Singleton instance of class used to report usage statistics.
+   *
+   * @var object
+   */
+   private $statHat;
+
+  /**
+   * A collection of tools used by all of the Message Broker applications.
+   *
+   * @var object
+   */
+   private $mbToolbox;
+
+  /**
+   *
+   */
+  function __construct($nid) {
+
+    $this->add($nid);
+
+    $this->mbConfig = MB_Configuration::getInstance();
+    $this->statHat = $this->mbConfig->getProperty('statHat');
+    $this->mbToolbox = $this->mbConfig->getProperty('mbToolbox');
+  }
+
+  /**
+   *
+   */
+  private function add() {
+
+  }
+
 }
