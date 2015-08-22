c96f364f0214b0c80d49667baba78a7a46587972
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-22 17:48:58 -0400

    Classes related to Messenger

---
+++ src/MBC_DigestEmail_MandrillMessenger.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MBC_DigestEmail_MandrillMessenger
+ * 
+ */
+
+namespace DoSomething\MBC_DigestEmail;
+
+
+/**
+ * MBC_DigestEmail_MandrillMessenger class - 
+ */
+class MBC_DigestEmail_MandrillMessenger extends MBC_DigestEmail_BaseMessenger {
+  
+
+
+    $user->merge_vars['FNAME'] = '';
+    $user->merge_vars['UNSUBSCRIBE_LINK'] = '';
+    
+    // Merge campaign template with campaign values
+    $user->merge_vars['CAMPAIGNS'] = '';
+
+    // build campaign list with divider markup
+
+    // tags
+    // subject line
+    // from_email
+    // from_name
+    // to (array)
+
+}
